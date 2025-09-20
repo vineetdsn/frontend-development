81931ed5d987dd6beb203e6f5f5a24892c051c26
vineetdsn <vineetdsn@gmail.com>
2025-09-20 23:51:14 +0530

    Adding javascript File

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-function badahoja(str){
-
-    let tenyears = str.toUpperCase()
-    return tenyears
+function displayDate(){
+    document.getElementById('demo').innerHTML = Date()
 }
-
-// console.log(badahoja("abcdef"))
-
-asurPandcard = {
-    asurAge: 99,
-    pancard: "iushdhf",
-    thisWala: function(str){
-
-        console.log("ye method he")
-        return str.toLowerCase()
-    }
-}
-
-let sahikar = 
-console.log(asurPandcard.pancard)
-// console.log(sahikar)
-
-console.log(badahoja(asurPandcard.pancard))
-console.log(asurPandcard.thisWala("KL"))
-// console.log(badahoja(asurPandcard.pancard))
-
-function show() {
-  console.log(this);
-}
-show()
