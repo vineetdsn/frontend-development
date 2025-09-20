ed6eab2fa66c94ada176734e591b1ec011fb5abd
vineetdsn <vineetdsn@gmail.com>
2025-09-21 00:49:02 +0530

    Element styles changes

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ function displayDate(){
 }
 
 function show(){
-    document.getElementById('para').innerHtml = "Pyari si bachi"
+    let para = document.getElementById('paragraph')
+    para.innerHTML = "Anya Pyari si bachi he"
+    para.style.display="block"
+    para.style.margin= "12px auto"
+}
+
+function changingStyle(){
+    let alter = document.getElementById('textBox');
+    alter.type = "textarea";
+    alter.style.border = "4px solid #19183b"
 }
