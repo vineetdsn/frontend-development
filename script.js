598ec8a4332e9e8255df158708e96b59e9d8963b
vineetdsn <vineetdsn@gmail.com>
2025-09-22 22:22:48 +0530

    Promise with catch

--- script.js
+++ script.js
@@ -149,13 +149,11 @@ fetch(apiUrl, requestOptions)
   });
 
 // Promise
-let promise = new Promise(function(resolve, reject) {
-  setTimeout(() => resolve("done!"), 1000);
+
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error("Whoops!")), 1000);
 });
 
-// resolve runs the first function in .then
-promise.then(
-  result => alert(result), // shows "done!" after 1 second
-  error => alert(error) // doesn't run
-);
+// .catch(f) is the same as promise.then(null, f)
+promise.catch(alert); // shows "Error: Whoops!" after 1 second
 
