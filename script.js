0aa7f9a6c45b38d4e3ac71e3b07f81bed5fc4dcb
vineetdsn <vineetdsn@gmail.com>
2025-09-22 22:21:49 +0530

    Promise

--- script.js
+++ script.js
@@ -148,3 +148,14 @@ fetch(apiUrl, requestOptions)
 ('Error:', error);
   });
 
+// Promise
+let promise = new Promise(function(resolve, reject) {
+  setTimeout(() => resolve("done!"), 1000);
+});
+
+// resolve runs the first function in .then
+promise.then(
+  result => alert(result), // shows "done!" after 1 second
+  error => alert(error) // doesn't run
+);
+
