4eb29ed03f48de814a5420f47c0247281e30e1ae
vineetdsn <vineetdsn@gmail.com>
2025-09-29 14:21:00 +0530

    resize js

--- script.js
+++ script.js
@@ -384,3 +384,16 @@ class Birthday {
     this.counter = 0
 
   }
+
+  
+  resize() {
+    this.width = canvas.width = window.innerWidth
+    let center = this.width / 2 | 0
+    this.spawnA = center - center / 4 | 0
+    this.spawnB = center + center / 4 | 0
+    
+    this.height = canvas.height = window.innerHeight
+    this.spawnC = this.height * .1
+    this.spawnD = this.height * .5
+    
+  }
