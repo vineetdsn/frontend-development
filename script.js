bb6e250872d9707b60dba535186b357ea8488128
vineetdsn <vineetdsn@gmail.com>
2025-10-01 00:02:54 +0530

    js  variables

--- script.js
+++ script.js
@@ -531,3 +531,12 @@ document.ontouchstart = evt => birthday.onClick(evt)
   	
 
   })()
+
+  // 
+  const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+msg.text = document.querySelector('[name="text"]').value;
