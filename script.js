3a572aa6ff61378af88c97ee4d3826ad779d3c62
vineetdsn <vineetdsn@gmail.com>
2025-10-03 00:44:29 +0530

    quiz data

--- script.js
+++ script.js
@@ -573,3 +573,38 @@ voicesDropdown.addEventListener("change", setVoice);
 options.forEach((option) => option.addEventListener("change", setOption));
 speakButton.addEventListener("click", toggle);
 stopButton.addEventListener("click", () => toggle(false));
+
+const quizData = [
+{
+question: "Which language runs in a web browser?",
+a: "Java",
+b: "C",
+c: "Python",
+d: "JavaScript",
+correct: "d",
+},
+{
+question: "What does CSS stand for?",
+a: "Central Style Sheets",
+b: "Cascading Style Sheets",
+c: "Cascading Simple Sheets",
+d: "Cars SUVs Sailboats",
+correct: "b",
+},
+{
+question: "What does HTML stand for?",
+a: "Hypertext Markup Language",
+b: "Hypertext Markdown Language",
+c: "Hyperloop Machine Language",
+d: "Helicopters Terminals Motorboats Lamborginis",
+correct: "a",
+},
+{
+question: "What year was JavaScript launched?",
+a: "1996",
+b: "1995",
+c: "1994",
+d: "none of the above",
+correct: "b",
+},
+];
