d3bec8a01c6e33392f3825c7c7ca576f099ff370
vineetdsn <vineetdsn@gmail.com>
2025-10-05 23:19:37 +0530

    removed quiz

--- script.js
+++ script.js
@@ -638,17 +638,3 @@ b_text.innerText = currentQuizData.b;
 c_text.innerText = currentQuizData.c;
 d_text.innerText = currentQuizData.d;
 };
-loadQuiz();
-const answer = getSelected();
-if (answer) {
-if (answer === quizData[currentQuiz].correct) score++;
-currentQuiz++;
-if (currentQuiz < quizData.length) loadQuiz();
-else {
-quiz.innerHTML = `
-<h2>You answered ${score}/${quizData.length} questions correctly</h2>
-<button onclick="history.go(0)">Play Again</button>
-` // location.reload() won't work in CodePen for security reasons;
-}
-}
-});
