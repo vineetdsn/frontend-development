96e7234db7c0143674abf968460eaeab8d360058
vineetdsn <vineetdsn@gmail.com>
2025-10-07 23:18:04 +0530

    removed de select function

--- script.js
+++ script.js
@@ -630,11 +630,4 @@ if (answerElement.checked) answer = answerElement.id;
 });
 return answer;
 };const loadQuiz = () => {
-deselectAnswers();
-const currentQuizData = quizData[currentQuiz];
-questionElement.innerText = currentQuizData.question;
-a_text.innerText = currentQuizData.a;
-b_text.innerText = currentQuizData.b;
-c_text.innerText = currentQuizData.c;
-d_text.innerText = currentQuizData.d;
-};
+
