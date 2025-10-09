694637780730360827a7f10cb578927f033491f3
vineetdsn <vineetdsn@gmail.com>
2025-10-09 21:59:26 +0530

    varibles extend

--- script.js
+++ script.js
@@ -512,14 +512,7 @@ class Firework {
 
 
   // 
-  const msg = new SpeechSynthesisUtterance();
-let voices = [];
-const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector("#speak");
-const stopButton = document.querySelector("#stop");
-msg.text = document.querySelector('[name="text"]').value;
-
+ 
 function populateVoices() {
   voices = this.getVoices();
   voicesDropdown.innerHTML = voices
