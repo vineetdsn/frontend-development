6e95c8e41da11aac223863b560324b2cd0b91bbe
vineetdsn <vineetdsn@gmail.com>
2025-10-10 19:34:57 +0530

    describe arc

--- script.js
+++ script.js
@@ -282,25 +282,6 @@ class Dial {
     };
   }
 
-  describeArc(x, y, radius, startAngle, endAngle) {
-    const start = this.polarToCartesian(x, y, radius, endAngle);
-    const end = this.polarToCartesian(x, y, radius, startAngle);
-    const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
-    const d = [
-      "M",
-      start.x,
-      start.y,
-      "A",
-      radius,
-      radius,
-      0,
-      largeArcFlag,
-      0,
-      end.x,
-      end.y
-    ].join(" ");
-    return d;
-  }
 
   setValue(value) {
     let c = (value / 100) * 360;
